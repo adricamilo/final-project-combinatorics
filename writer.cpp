3016f62739a77f5124efccc2e378b24ce483f007
adricamilo <adrian.vasconez@gmail.com>
2022-12-12 20:54:43 -0500

    no spaces

--- writer.cpp
+++ writer.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 void writeStatus(vector<int>& lista, ofstream& writer) {
-	for (int i = 0; i < lista.size(); i++) {
-		writer << lista[i];
-		if (i != lista.size() - 1)
-			writer << ", ";
-		else
-			writer << endl;
-	}
+    for (int i = 0; i < lista.size(); i++) {
+        writer << lista[i];
+        if (i != lista.size() - 1)
+            writer << ",";
+        else
+            writer << endl;
+    }
 }
